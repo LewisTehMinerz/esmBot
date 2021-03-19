c5329accda248f6c3dd832e379a90df349580cfb
Lewis Crichton <lewistehminerz@outlook.com>
2021-03-19 15:57:25 +0000

    drop old constants

--- commands/retro.js
+++ commands/retro.js
@@ -1,5 +1,4 @@
 const RetroText = require("retrotext");
-const { TextStyle, BackgroundStyle } = RetroText;
 
 exports.run = async (message, args) => {
   if (args.length === 0) return `${message.author.mention}, you need to provide some text to generate some retro text!`;
@@ -8,11 +7,11 @@ exports.run = async (message, args) => {
   if (/^[\w ]+$/i.test(line1) === false || /^[\w ]+$/i.test(line2) === false || /^[\w ]+$/i.test(line3) === false) return `${message.author.mention}, only alphanumeric characters, spaces, and underscores are allowed!`;
   let text;
   if (line3) {
-    text = new RetroText().setLine1(line1).setLine2(line2).setLine3(line3).setBackgroundStyle(BackgroundStyle.OUTLINE_TRIANGLE).setTextStyle(TextStyle.CHROME);
+    text = new RetroText().setLine1(line1).setLine2(line2).setLine3(line3).setBackgroundStyle("outlineTri").setTextStyle("chrome");
   } else if (line2) {
-    text = new RetroText().setLine1(line1).setLine2(line2).setBackgroundStyle(BackgroundStyle.OUTLINE_TRIANGLE).setTextStyle(TextStyle.CHROME);
+    text = new RetroText().setLine1(line1).setLine2(line2).setBackgroundStyle("outlineTri").setTextStyle("chrome");
   } else {
-    text = new RetroText().setLine2(line1).setBackgroundStyle(BackgroundStyle.OUTLINE_TRIANGLE).setTextStyle(TextStyle.CHROME);
+    text = new RetroText().setLine2(line1).setBackgroundStyle("outlineTri").setTextStyle("chrome");
   }
   const buffer = await text.fetchBuffer();
   return {
